e94cc55cab560da51794830feb25b2e6fa1e649b
loucas monir <loucasmonir@gmail.com>
2026-06-02 15:15:30 +0300

    register cookie paster for fastify and change port

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,12 +4,16 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import cookieParser from '@fastify/cookie';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.register(cookieParser);
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
