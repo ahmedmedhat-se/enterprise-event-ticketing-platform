54534b6575653cd457b89422e429254b8f53a7ce
loucas monir <loucasmonir@gmail.com>
2026-06-03 21:18:12 +0300

    add validation a pipe globally

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import cookieParser from '@fastify/cookie';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -13,6 +14,14 @@ async function bootstrap() {
   );
 
   await app.register(cookieParser);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 4000);
 }
