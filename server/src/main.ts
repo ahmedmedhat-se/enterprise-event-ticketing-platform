02bc7aa888a8d027fbaca3ff162804361369d1b4
loucas monir <loucasmonir@gmail.com>
2026-07-14 21:05:01 +0300

    add WebSocket adapter to the app

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from './app.module';
 import cookieParser from '@fastify/cookie';
 import { ValidationPipe } from '@nestjs/common';
@@ -12,6 +13,10 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+
+  // Register WebSocket adapter using native WebSockets (ws library)
+  // WsAdapter attaches to the underlying Node.js HTTP server created by Fastify
+  app.useWebSocketAdapter(new WsAdapter(app));
 
   await app.register(cookieParser);
 
